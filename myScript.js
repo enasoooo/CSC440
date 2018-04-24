a9e1492f266929a16e48c0bf3e1af0ff4354574f
enasoooo <38050992+enasoooo@users.noreply.github.com>
2018-04-24 21:44:10 +0300

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,33 @@
+<html>
+<body>
+<script>
+    
+function getValue() {
+
+var x = document.getElementById("foo").value;
+var z = document.getElementById("koo").value;
+"------------------------------------------------------";
+var y = document.getElementById("boo");
+var zs = document.getElementById("moo").value;
+"------------------------------------------------------";
+var y = document.getElementById("boo");
+var xc = document.getElementById("noo").value;
+
+"------------------------------------------------------";
+var y = document.getElementById("boo");
+var tt = document.getElementById("ooo").value;
+"------------------------------------------------------";
+
+var y = document.getElementById("boo");
+var q = document.getElementById("qoo").value;
+document.write( "The event will organized by the organizer:" + x +'   ' + z+" The event  name:" + zs+" The event  Location:" + xc+ " The event  Date:" + tt+" The event  contact:" + q+"<br>");
+"------------------------------------------------------";
+}
+
+</script>
+  </body>
+</html>
+
+
+
+<script src="myScript.js"></script>
